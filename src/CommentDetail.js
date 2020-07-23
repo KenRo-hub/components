040d2ad8769ca13eb42dd483a18b059ec193a465
Edgar Kenyi Rojo Moreno <edgar.kenyi.rojo@gmail.com>
2020-07-23 15:57:49 -0500

    commit

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,19 +1,17 @@
 import React from "react";
-import faker from "faker";
 
 const CommentDetail = (props) => {
-    
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()} />
+        <img alt="avatar" src={props.img} />
       </a>
       <div className="content">
         <a href="/" className="author">
           {props.author}
         </a>
         <div className="metadata">
-          <span className="date">Today at 6:00 pm</span>
+          <span className="date">{props.timeAgo}</span>
         </div>
         <div className="text">{props.comment}</div>
       </div>
--- src/index.js
+++ src/index.js
@@ -2,13 +2,45 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Cheo" comment="Hola" />
-      <CommentDetail author="Nox" comment="Señor" />
-      <CommentDetail author="Johannes" comment="Como estas?" />
+      <ApprovalCard>
+        <div>
+          <h2 align="center" color="red">
+            Warning!
+          </h2>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Cheo"
+          timeAgo="Today at 4:45 PM"
+          comment="Hi"
+          img={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Nox"
+          timeAgo="Yesterday at 12:25 PM"
+          comment="Ken"
+          img={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Johannes"
+          timeAgo="Monday at 01:30 AM"
+          comment="Follow your dreams"
+          img={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
